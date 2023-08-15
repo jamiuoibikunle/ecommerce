2034dd0cfb500d441bf0c180993a76165dd9434f
Jamiu Ibikunle <ibidotun@gmail.com>
2023-08-15 07:40:29 +0100

    tablet sendinquiry

--- src/components/home/SendInquiry.tsx
+++ src/components/home/SendInquiry.tsx
@@ -7,10 +7,16 @@ import {
   Heading,
   Image,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
   Select,
   Stack,
   Text,
   Textarea,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
 import { BsArrowRight, BsChevronRight } from "react-icons/bs";
@@ -31,6 +37,8 @@ const SendInquiry = () => {
 };
 
 const Mobile = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Stack
       bgImage="/sendinquiry.png"
@@ -43,10 +51,52 @@ const Mobile = () => {
       py={10}
       spacing={8}
     >
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size="sm">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>
+            <ModalCloseButton />
+            <Stack py={10} spacing={5}>
+              <Stack>
+                <Heading fontSize="1.5rem" fontWeight={500} textAlign="center">
+                  An easy way to send requests to all suppliers
+                </Heading>
+                <Text textAlign="center">
+                  Lorep ipsum dolor sit amet, consecteturadipsicing elit, sed do
+                  elusmod tempor incidunt.
+                </Text>
+              </Stack>
+              <Stack
+                p={5}
+                borderRadius={5}
+                spacing={5}
+                borderWidth={1}
+                borderColor="gray.300"
+              >
+                <Heading fontSize="1.5rem" fontWeight={500}>
+                  Send quote to suppliers
+                </Heading>
+                <Input placeholder="What item do you need?" />
+                <Textarea placeholder="Type more details" resize="none" />
+                <HStack>
+                  <Input w="12rem" placeholder="Quantity" />
+                  <Select w="6rem">
+                    <option>pcs</option>
+                    <option>kg</option>
+                  </Select>
+                </HStack>
+                <Button colorScheme="blue" w="fit-content">
+                  Send inquiry
+                </Button>
+              </Stack>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
       <Heading fontSize="2rem" fontWeight={500}>
         An easy way to send requests to all suppliers
       </Heading>
-      <Button colorScheme="blue" w="fit-content">
+      <Button colorScheme="blue" w="fit-content" onClick={onOpen}>
         Send inquiry
       </Button>
     </Stack>
@@ -54,7 +104,57 @@ const Mobile = () => {
 };
 
 const Tablet = () => {
-  return <>Tablet</>;
+  return (
+    <Flex
+      justifyContent="space-between"
+      bgImage="/sendinquiry.png"
+      bgPos="center"
+      bgSize="cover"
+      w="100%"
+      maxW="7xl"
+      borderWidth={1}
+      borderColor="gray.300"
+      borderRadius={5}
+      px={5}
+      py={10}
+      mt={3}
+    >
+      <Stack w="sm" color="white">
+        <Heading fontSize="2rem" fontWeight={500}>
+          An easy way to send requests to all suppliers
+        </Heading>
+        <Text>
+          Lorep ipsum dolor sit amet, consecteturadipsicing elit, sed do elusmod
+          tempor incidunt.
+        </Text>
+      </Stack>
+      <Stack
+        bg="white"
+        p={5}
+        borderRadius={5}
+        spacing={5}
+        w="sm"
+        borderWidth={1}
+        borderColor="gray.300"
+      >
+        <Heading fontSize="1.5rem" fontWeight={500}>
+          Send quote to suppliers
+        </Heading>
+        <Input placeholder="What item do you need?" />
+        <Textarea placeholder="Type more details" />
+        <HStack>
+          <Input w="12rem" placeholder="Quantity" />
+          <Select w="6rem">
+            <option>pcs</option>
+            <option>kg</option>
+          </Select>
+        </HStack>
+        <Button colorScheme="blue" w="fit-content">
+          Send inquiry
+        </Button>
+      </Stack>
+    </Flex>
+  );
 };
 
 const Desktop = () => {
