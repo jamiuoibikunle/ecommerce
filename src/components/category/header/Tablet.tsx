6961144a280a04c96ccd2acdda56e54e319d95cb
Jamiu Ibikunle <ibidotun@gmail.com>
2023-08-17 18:07:32 +0100

    completed header in category

--- src/components/category/header/Tablet.tsx
+++ src/components/category/header/Tablet.tsx
@@ -7,16 +7,24 @@ import {
   InputLeftElement,
   Spacer,
   Stack,
+  Text,
 } from "@chakra-ui/react";
 import { BsArrowLeft, BsCart, BsPerson, BsSearch } from "react-icons/bs";
+import { handlePaths } from "@/utils/handlePaths";
+import { usePathname } from "next/navigation";
 
 const Tablet = () => {
+  const paths = usePathname();
+  const resolved = handlePaths(paths as string);
+  const title = resolved[resolved.length - 1].replaceAll("%20", " ");
+
   return (
     <Stack px={5}>
       <HStack py={5} spacing={5}>
         <Box cursor="pointer">
           <BsArrowLeft size={30} />
         </Box>
+        <Text fontWeight={600}>{title}</Text>
         <Spacer />
         <BsCart size={25} />
         <BsPerson size={25} />
