96b8906c35baf12ec35c71d7c10848df95edc3f8
Jamiu Ibikunle <ibidotun@gmail.com>
2023-08-18 08:02:29 +0100

    price range

--- src/components/category/FilterOperators.tsx
+++ src/components/category/FilterOperators.tsx
@@ -9,8 +9,15 @@ import {
   Box,
   Button,
   Checkbox,
+  FormControl,
+  FormLabel,
   HStack,
   Heading,
+  Input,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
   Stack,
   Text,
 } from "@chakra-ui/react";
@@ -23,6 +30,8 @@ const FilterOpeators = () => {
       <Accordion allowToggle allowMultiple defaultIndex={[0, 1]}>
         <SubCategory />
         <Brands />
+        <Features />
+        <PriceRange />
       </Accordion>
     </Stack>
   );
@@ -98,9 +107,6 @@ const SubCategory = () => {
 };
 
 const Brands = () => {
-  const path = usePathname();
-  const router = useRouter();
-
   const [brands, setBrands] = useState([
     { title: "Samsung" },
     { title: "Apple" },
@@ -155,6 +161,113 @@ const Brands = () => {
               </Text>
             )}
           </Button>
+        </Stack>
+      </AccordionPanel>
+    </AccordionItem>
+  );
+};
+
+const Features = () => {
+  const [brands, setBrands] = useState([
+    { title: "Metallic" },
+    { title: "Plastic cover" },
+    { title: "8GB Ram" },
+    { title: "Super power" },
+    { title: "Large memory" },
+    { title: "5G network" },
+    { title: "4000mAh battery" },
+    { title: "Micro sim" },
+  ]);
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <AccordionItem borderWidth={0} pb={4}>
+      <AccordionButton px={0}>
+        <Box as="span" flex="1" textAlign="left">
+          <Heading fontSize="1rem" fontWeight={600}>
+            Features
+          </Heading>
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+      <AccordionPanel px={0}>
+        <Stack spacing={isExpanded ? 5 : 0}>
+          <Stack
+            spacing={5}
+            maxHeight={isExpanded ? "fit-content" : "10.5rem"}
+            overflowY="hidden"
+          >
+            {brands.map((item, index) => {
+              return (
+                <Checkbox key={index} w="100%">
+                  <Text w="100%" textAlign="left" color="blackAlpha.600">
+                    {item.title}
+                  </Text>
+                </Checkbox>
+              );
+            })}
+          </Stack>
+          <Button variant="link" fontWeight={500} onClick={toggleExpanded}>
+            {isExpanded ? (
+              <Text w="100%" textAlign="left" color="blue.500">
+                See less
+              </Text>
+            ) : (
+              <Text w="100%" textAlign="left" color="blue.500">
+                See all
+              </Text>
+            )}
+          </Button>
+        </Stack>
+      </AccordionPanel>
+    </AccordionItem>
+  );
+};
+
+const PriceRange = () => {
+  return (
+    <AccordionItem borderWidth={0} pb={4}>
+      <AccordionButton px={0}>
+        <Box as="span" flex="1" textAlign="left">
+          <Heading fontSize="1rem" fontWeight={600}>
+            Price Range
+          </Heading>
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+      <AccordionPanel px={0}>
+        <Stack py={3} spacing={4}>
+          <Slider aria-label="price-range-slider" defaultValue={30}>
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+          <Stack>
+            <HStack w="100%" spacing={2}>
+              <FormControl w="50%">
+                <FormLabel>Min</FormLabel>
+                <Input type="number" placeholder="0" />
+              </FormControl>
+              <FormControl w="50%">
+                <FormLabel>Max</FormLabel>
+                <Input type="number" placeholder="999999" />
+              </FormControl>
+            </HStack>
+            <Button
+              size="md"
+              variant="outline"
+              colorScheme="gray"
+              color="blue"
+              fontWeight={500}
+            >
+              Apply
+            </Button>
+          </Stack>
         </Stack>
       </AccordionPanel>
     </AccordionItem>
