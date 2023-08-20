6baa9bc5ccf0a34dce984e2a91fa7cdfb1e9fe77
Jamiu Ibikunle <ibidotun@gmail.com>
2023-08-20 11:24:20 +0100

    spreadQueries all right

--- src/utils/handleQuery.ts
+++ src/utils/handleQuery.ts
@@ -1,5 +1,28 @@
 import { Query } from "@/types"
 
 export const handleQuery = (query: Query, key: string, item: string) => {
-    console.log(query, item)
+    // Sort other queries
+    const others = spreadQueries(query, key)
+    console.log(others)
+
+    // Check if key exists
+    const keyExists = query[key];
+    if (keyExists) {
+    }
+    console.log(query)
+
+}
+
+const spreadQueries = (query: Query, foreignkey: string) => {
+    let resolved = "";
+    let count = 0;
+
+    delete query[foreignkey]
+
+    for (const [key, value] of Object.entries(query)) {
+        count === 0 ? resolved += `?${key}=${value}` : resolved += `&${key}=${value}`;
+        count++;
+    }
+
+    return resolved
 }
